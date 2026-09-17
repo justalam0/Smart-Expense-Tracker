b7b1c460a12830a4c55bb66e431cb6b6e97291be
justalam0 <mmehtabalam0127@gmail.com>
2026-09-17 13:02:41 +0530

    Updated API URL

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://smart-expense-tracker-vrxs.onrender.com/api',
 });
 
 // Automatically attach the token to every request
